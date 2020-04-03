eb66087e7042905d9d5560b77ef8698030249a71
Gia Edgington <gia.edgington@gmail.com>
2020-04-02 20:14:28 -0400

    Create dummy user

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(2)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -29,9 +38,19 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 sequelize
-.sync({ force: true})
+.sync()
 .then(result => {
+    return User.findByPk(2);
     //console.log(result);
+})
+.then(user => {
+    if (!user) {
+        return User.create({ username: 'April', email: 'test@test.com'});
+    }
+    return user;
+})
+.then(user => {
+    //console.log(user);
     app.listen(3000);
 })
 .catch(err => {
